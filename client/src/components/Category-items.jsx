ce11a8109493157b591e02d56075f99baf78a49c
Greg Bailey <gregbailey617@gmail.com>
2025-01-18 17:41:33 -0500

    messed up case sensitive stuf github

--- client/src/components/Category-items.jsx
+++ client/src/components/Category-items.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import ProductCard from './Product-Card';
 
-//changed name
-
 function CategoryItems (props) {
 
   const [productsDisplayState, setProductsDisplayState] = useState([]);
