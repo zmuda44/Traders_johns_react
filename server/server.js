b7610f03b7cc45e4abb26f14dad4e75825a4a4c4
Greg Bailey <gregbailey617@gmail.com>
2025-02-14 10:23:42 -0500

    still trying to fix render

--- server/server.js
+++ server/server.js
@@ -40,12 +40,12 @@ app.use(session(sess));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  // app.use(express.static(path.join(__dirname, '../client/dist')));
   
-  // app.get("*", (req, res) => {
-  //   // For any routes not handled by the server, send the main index.html file
-  //   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  // });
+  app.get("*", (req, res) => {
+    // For any routes not handled by the server, send the main index.html file
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 }
 
 // app.use(cors({
